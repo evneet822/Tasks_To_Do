1aa6fbb6de69772fa1225a708a758a8b031f1b6b
evneet <nancymaan4@gmail.com>
2020-01-21 19:06:21 -0500

    almost done

--- TasksToDo/Controller/TaskTableViewController.swift
+++ TasksToDo/Controller/TaskTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class TaskTableViewController: UITableViewController {
+class TaskTableViewController: UITableViewController,UISearchBarDelegate {
     
     
     var contextEntity : NSManagedObjectContext?
@@ -24,6 +24,8 @@ class TaskTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        
         
         let appdeleagte = UIApplication.shared.delegate as! AppDelegate
         let context = appdeleagte.persistentContainer.viewContext
@@ -50,6 +52,14 @@ class TaskTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
          let cell = tableView.dequeueReusableCell(withIdentifier: "tasksCell")
             cell?.textLabel?.text = tasks![indexPath.row].value(forKey: "title") as? String
+        cell?.detailTextLabel?.text = "Days worked: \(tasks![indexPath.row].value(forKey: "daysAdded") as! Int)"
+        cell?.backgroundColor = .white
+        let addedDays = tasks![indexPath.row].value(forKey: "daysAdded") as! Int
+        let neededDays = tasks![indexPath.row].value(forKey: "daysNeeded") as! Int
+        if addedDays >= neededDays{
+            cell?.backgroundColor = .cyan
+        }
+        
             return cell!
         
         
@@ -95,15 +105,96 @@ class TaskTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if let destination = segue.destination as? ViewController{
+            if let cell = sender as? UITableViewCell{
+                let taskTitle = tasks![tableView.indexPath(for: cell)!.row].value(forKey: "title") as! String
+                destination.titleVC = taskTitle
+            }
+        }
     }
-    */
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "TasksEntity")
+                   request.predicate = NSPredicate(format: "title contains[c] %@", searchText)
+                   
+                   do{
+                       let result = try contextEntity?.fetch(request)
+                       tasks = (result as! [NSManagedObject])
+                   }catch{
+                       print(error)
+                   }
+                   tableView.reloadData()
+        if searchText == ""{
+            loadCoreData()
+        }
+        
+        
+       
+    
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let title = self.tasks![indexPath.row].value(forKey: "title") as! String
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "TasksEntity")
+        request.predicate = NSPredicate(format: "title contains %@", title)
+        request.returnsObjectsAsFaults = false
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, success) in
+            
+            do{
+                let data = try self.contextEntity?.fetch(request)
+                            for ob in data as! [NSManagedObject]{
+                                self.contextEntity?.delete(ob)
+                                self.tasks?.remove(at: indexPath.row)
+                                tableView.deleteRows(at: [indexPath], with: .fade)
+            //                    print("item deleted")
+                            }
+                        }catch{
+                            print("item not deleted")
+                        }
+                        
+                        do {
+                            try self.contextEntity?.save()
+                            self.loadCoreData()
+                        }catch{
+                            print("not saved")
+                        }
+        }
+        
+        let shareAction = UIContextualAction(style: .normal, title: "Add Day") { (action, view, success) in
+            do{
+                let data = try self.contextEntity?.fetch(request)
+                for ob in data as! [NSManagedObject]{
+                    var days = ob.value(forKey: "daysAdded") as! Int
+                    days += 1
+                    ob.setValue(days, forKey: "daysAdded")
+                    }
+            }catch{
+                print(error)
+            }
+            do{
+                try self.contextEntity?.save()
+                self.loadCoreData()
+            }catch{
+                print(error)
+            }
+            
+        }
+        
+        shareAction.backgroundColor = .gray
+        return UISwipeActionsConfiguration(actions: [deleteAction,shareAction])
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         loadCoreData()
@@ -115,7 +206,7 @@ class TaskTableViewController: UITableViewController {
         
         do{
             let result = try contextEntity?.fetch(request)
-            tasks = result as! [NSManagedObject]
+            tasks = (result as! [NSManagedObject])
         }catch{
             print(error)
         }
@@ -134,6 +225,44 @@ class TaskTableViewController: UITableViewController {
         }catch{
             print(error)
         }
+        do{
+            try contextEntity?.save()
+        }catch{
+            print(error)
+        }
     }
-
+    
+    
+    @IBAction func sortByDate(_ sender: UIBarButtonItem) {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "TasksEntity")
+        request.sortDescriptors = [NSSortDescriptor(key: "taskStarted", ascending: false)]
+        do{
+            
+            let result = try contextEntity?.fetch(request)
+            tasks = (result as! [NSManagedObject])
+        
+        }catch{
+            print(error)
+        }
+        tableView.reloadData()
+        
+    }
+    
+    
+    @IBAction func sortByTitle(_ sender: UIBarButtonItem) {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "TasksEntity")
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        do{
+            
+            let result = try contextEntity?.fetch(request)
+            tasks = (result as! [NSManagedObject])
+        
+        }catch{
+            print(error)
+        }
+        tableView.reloadData()
+        
+    }
+    
 }
--- TasksToDo/Controller/ViewController.swift
+++ TasksToDo/Controller/ViewController.swift
@@ -15,12 +15,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var titletxt: UITextField!
     @IBOutlet weak var daystxt: UITextField!
     @IBOutlet weak var descfeild: UITextView!
+    var titleVC = ""
+    var selectedTask : NSManagedObject?
     
    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+               let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "TasksEntity")
+        request.predicate = NSPredicate(format: "title contains %@", titleVC)
+        request.returnsObjectsAsFaults = false
+        do{
+            let data = try context.fetch(request)
+            for object in data as! [NSManagedObject] {
+                selectedTask = object
+                titletxt.text = (object.value(forKey: "title") as! String)
+                daystxt.text = "\(object.value(forKey: "daysNeeded") as? Int ?? 0)"
+                descfeild.text = (object.value(forKey: "taskDescription") as! String)
+            }
+            
+        }catch{
+            print(error)
+        }
+        let tapgesture = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        view.addGestureRecognizer(tapgesture)
+        
     }
 
     @IBAction func addTasks(_ sender: UIButton) {
@@ -29,34 +51,68 @@ class ViewController: UIViewController {
         let daysneeded = Int(daystxt.text ?? "0") ?? 0
         let desc = descfeild.text
         
+        
+        if title == "" && daysneeded == 0{
+           
+            let alert = UIAlertController(title: "Empty feilds", message: "Fill all the feilds", preferredStyle: .alert)
+                   let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                   alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+        else{
+            
+        }
+        
         let appdelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appdelegate.persistentContainer.viewContext
         
-        
-        
-        let taskentity = NSEntityDescription.insertNewObject(forEntityName: "TasksEntity", into: context)
-        
-        taskentity.setValue(title, forKey: "title")
-        taskentity.setValue(daysneeded, forKey: "daysNeeded")
-        taskentity.setValue(0, forKey: "daysAdded")
-        taskentity.setValue(desc, forKey: "taskDescription")
-        
-        do{
-            try context.save()
-        }catch{
-            print(error)
+        if selectedTask == nil{
+            let taskentity = NSEntityDescription.insertNewObject(forEntityName: "TasksEntity", into: context)
+                   
+                   taskentity.setValue(title, forKey: "title")
+                   taskentity.setValue(daysneeded, forKey: "daysNeeded")
+                   taskentity.setValue(0, forKey: "daysAdded")
+                   taskentity.setValue(desc, forKey: "taskDescription")
+            taskentity.setValue(NSDate(), forKey: "taskStarted")
+            
+                   
+                   do{
+                       try context.save()
+                   }catch{
+                       print(error)
+                   }
         }
+        
+        if selectedTask != nil{
+            selectedTask?.setValue(title, forKey: "title")
+            selectedTask?.setValue(daysneeded, forKey: "daysNeeded")
+            selectedTask?.setValue(desc, forKey: "taskDescription")
+            
+            do{
+                try context.save()
+            }catch{
+                print(error)
+            }
+        }
+        
         titletxt.text = ""
         daystxt.text = ""
         descfeild.text = ""
+        titletxt.resignFirstResponder()
+        daystxt.resignFirstResponder()
+        descfeild.resignFirstResponder()
          
-        
-        
-        
         
     }
     
+    @objc func tapped()  {
+        titletxt.resignFirstResponder()
+        daystxt.resignFirstResponder()
+        descfeild.resignFirstResponder()
+    }
+    
    
+  
     
 }
 
